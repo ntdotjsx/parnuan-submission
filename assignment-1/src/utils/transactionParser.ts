ed9fead9c5a42b4f8b982b6816abde09d08a86e5
ntdotjsx <0ms.ntdotjsx@gmail.com>
2026-08-13 23:06:05 +0700

    add: types transactionParser.ts

---
+++ assignment-1/src/utils/transactionParser.ts
@@ -0,0 +1,13 @@
+export type Category = {
+    id: string
+    title: string
+}
+
+export type Transaction = {
+    id: string
+    description: string
+    amount: number
+    category: Category
+    date: string
+    // confidence: number //ความมั่นใจในการจัดหมวดหมู่ของรายการธุรกรรม
+}
